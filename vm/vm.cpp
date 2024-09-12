107bc5af0ef04f56cb71b42b80d9bc8e42ee09d9
MartinLwx <MartinLwx@163.com>
2024-09-12 23:22:28 +0800

    Finish 15.2 A Value Stack Manipulator

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -1,5 +1,7 @@
 #include "vm.h"
 
+// TODO: Is Pointer Copy a good thing or not?
+//       What's the better way?
 InterpretResult VM::interpret(Chunk *chunk) {
   chunk_ = chunk;
   ip_ = 0;
--- vm/vm.h
+++ vm/vm.h
@@ -1,6 +1,7 @@
 #ifndef CPPLOX_VM_H
 #define CPPLOX_VM_H
 
+#include <vector>
 #define DEBUG_TRACE_EXECUTION
 #include "debug/debug.h"
 #include "instruction/chunk.h"
@@ -19,18 +20,28 @@ public:
 private:
   Chunk *chunk_;
   std::uint8_t ip_;
+  std::vector<Value> stack_;
+
   InterpretResult run() {
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+      std::cout << "          ";
+      for (const auto &slot : stack_) {
+        std::cout << "[ " << slot << " ]";
+      }
+      std::cout << "\n";
       disassembleInstruction(chunk_, ip_);
 #endif
       auto instruction{chunk_->getCode()[ip_++]};
       switch (instruction) {
-      case OP_RETURN:
+      case OP_RETURN: {
+        auto last{stack_[stack_.size() - 1]};
+        std::cout << last << "\n";
         return InterpretResult::INTERPRET_OK;
+      }
       case OP_CONSTANT: {
         auto constant{chunk_->getValueArray()[chunk_->getCode()[ip_++]]};
-        std::cout << constant << "\n";
+        stack_.emplace_back(constant);
         break;
       }
       }
